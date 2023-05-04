1959b5864f8f2d93c5d0ae5f4108dbefa75da1c1
unknown <kinchoily@hotmail.com>
2023-05-04 18:11:58 +0100

    DateTimeTile.js - Move logic for formatting date and time into custom hook

--- src/components/Tiles/DateTimeTile/DateTimeTile.js
+++ src/components/Tiles/DateTimeTile/DateTimeTile.js
@@ -1,21 +1,13 @@
+import useDate from "../../../hooks/useDate";
 import "./DateTimeTile.scss";
 
 const DateTimeTile = () => {
-  const dateObj = new Date();
-
-  const weekday = dateObj.toLocaleString("default", { weekday: "long" });
-  const date = dateObj.getDate().toLocaleString();
-  const month = dateObj.toLocaleString("default", { month: "long" });
-
-  const time = dateObj.toLocaleTimeString("default", {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+  const { dateFull, time } = useDate();
 
   return (
     <div className="dateTime">
       <div className="dateTime__date">
-        <p>{`${weekday} ${date} ${month}`}</p>
+        <p>{dateFull}</p>
       </div>
       <div className="dateTime__time">
         <p>{time}</p>
