70ed09edb401acefd4b627d66453905ca5ff2085
unknown <kinchoily@hotmail.com>
2023-05-09 16:58:22 +0100

    SearchBar.js - Add logic for handling user search query

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,13 +1,34 @@
+import { useState } from 'react';
 import { AiOutlineSearch } from "react-icons/ai";
 import "./SearchBar.scss";
 
 const SearchBar = (props) => {
+  const [query, setQuery] = useState("");
+
+  // store search query
+  const storeInputHander = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const clearInputHandler = () => {
+    setQuery("");
+  }
+
   return (
-    <form className="searchBar">
+    <form
+      className="searchBar"
+      onSubmit={(e) => {
+        props.handleSubmit(e, query);
+        clearInputHandler();
+      }}
+    >
       <input
         type="text"
         placeholder="Search Location"
         className="searchBar__input"
+        required
+        onChange={storeInputHander}
+        value={query}
       />
       <button className="searchBar__btn">
         <AiOutlineSearch />
