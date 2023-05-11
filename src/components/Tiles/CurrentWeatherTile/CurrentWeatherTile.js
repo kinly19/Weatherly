f7813408578e62677334773e5fa42852a52b951d
unknown <kinchoily@hotmail.com>
2023-05-11 17:28:01 +0100

    Create 'CurrentWeatherTile' component

---
+++ src/components/Tiles/CurrentWeatherTile/CurrentWeatherTile.js
@@ -0,0 +1,26 @@
+import useDate from '../../../hooks/useDate';
+import IconTile from '../IconTile/IconTile';
+// import {Clear, ClearNight} from '../../../assets/Svg/Svg.js';
+import './CurrentWeatherTile.scss';
+  
+const CurrentWeatherTile = (props) => {
+  // Custom hook
+  const { dateFull: date } = useDate(props.datetimeEpoch);
+
+  return (
+    <section className={props.class ? `current-info ${props.class}` : 'current-info'}>
+      <p className="current-info__location">{props.resolvedAddress}</p>
+      <article className="current-info__tile">
+        <IconTile 
+          iconSrc={props.icon} 
+          height={"12rem"} 
+        />
+        <p className="current-info__tile-temp">{`${props.temp} C`}</p>
+        <p className="current-info__tile-description">{`${props.conditions}`}</p>
+      </article>
+      <p className="current-info__date">{date}</p>
+    </section>
+  );
+};
+
+export default CurrentWeatherTile;
