a873bf82674409f68edd124050adf3a500066563
unknown <kinchoily@hotmail.com>
2023-05-17 15:13:45 +0100

    LayoutMain.js - Update prop name

--- src/components/Layout/LayoutMain.js
+++ src/components/Layout/LayoutMain.js
@@ -7,7 +7,7 @@ const LayoutMain = (props) => {
 
   return (
     <div className="layout__background" style={background}>
-      <main className={props.class ? `layout__container ${props.class}` : 'layout__container'}>
+      <main className={props.className ? `layout__container ${props.className}` : 'layout__container'}>
         {props.children}
       </main>
     </div>
