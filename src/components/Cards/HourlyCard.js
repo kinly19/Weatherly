f420d9cc33fcf6a05c70ed4cf812373f3fdb1231
unknown <kinchoily@hotmail.com>
2023-05-17 15:26:30 +0100

    Create 'HourlyCard' component

---
+++ src/components/Cards/HourlyCard.js
@@ -0,0 +1,47 @@
+import useDate from '../../hooks/useDate';
+import StatTile from '../Tiles/StatTile/StatTile';
+import './HourlyCard.scss'; 
+
+const HourlyCard = (props) => {
+
+  const { time } = useDate(props.datetimeEpoch);
+
+  const statTiles = [
+    { title: "Hour", value: props.datetimeEpoch},
+    { title: "Conditions", value: props.conditions},
+    { title: "Rain", value: props.precipprob },
+    { title: "Temp", value: props.temp },
+    { title: "Feels like", value: props.feelslike },
+    { title: "Humidity", value: props.humidity },
+    { title: "Cloud cover", value: props.cloudcover },
+    { title: "Wind", value: props.windspeed },
+    { title: "Direction", value: props.winddir },
+    { title: "Visibility", value: props.visibility },
+  ];
+
+  return (
+    <li className="hourly-card">
+      <div className="hourly-card__wrapper">
+        <div className="hourly-card__header">
+          <p className="hourly-card__header-time">{time}</p>
+          <p className="hourly-card__header-description">{statTiles[1].value}</p>
+        </div>
+
+        <ul className="hourly-card__list">
+          {statTiles.slice(2).map((tile, index) => {
+            return (
+              <StatTile
+                key={index}
+                class={"stat-tile--pd-sm"}
+                title={tile.title}
+                value={tile.value}
+              />
+            );
+          })}
+        </ul>
+      </div>
+    </li>
+  );
+};
+
+export default HourlyCard;
