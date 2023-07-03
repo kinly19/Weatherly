5a204a466eb23caf2fd4a57c08b1dfb4e66f55fe
unknown <kinchoily@hotmail.com>
2023-07-03 21:59:14 +0100

    Overlay.js - Add side effect to block page scroll when overlay is open

--- src/components/Overlay/Overlay.js
+++ src/components/Overlay/Overlay.js
@@ -1,16 +1,23 @@
+import { useEffect } from 'react';
 import { AiOutlineClose } from 'react-icons/ai'
 import './Overlay.scss'; 
 
 const Overlay = (props) => {
-  const isToggled = props.isToggled
+  const isToggled = props.isToggled;
+  // Block scroll when overlay is open
+  useEffect(() => {
+    if (isToggled) document.body.style.overflow = "hidden";
+    return () => (document.body.style.overflow = "unset");
+  }, [isToggled]);
+
   return (
     <section className={isToggled ? "overlay" : "overlay overlay--hidden"}>
-      <div className='overlay__toggle'>
-        <button className="overlay__toggle-btn" onClick={props.onClick}><AiOutlineClose /></button>
+      <div className="overlay__toggle">
+        <button className="overlay__toggle-btn" onClick={props.onClick}>
+          <AiOutlineClose />
+        </button>
       </div>
-      <div className='overlay__content'>
-        {props.children}
-      </div>
+      <div className="overlay__content">{props.children}</div>
     </section>
   );
 };
