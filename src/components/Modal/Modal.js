4cf7423645257b4c01ca3607dadba429e5bf52bf
unknown <kinchoily@hotmail.com>
2023-07-05 16:32:47 +0100

    Rename file 'Overlay.js' > 'Modal.js' & update logic to handle modals toggle state

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,41 @@
+import { useState, useEffect, forwardRef, useImperativeHandle } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
+import './Modal.scss'; 
+
+// ForwardsRef allows us to tell react when calling useRef(in parent), ref should point to this child component
+const Modal = forwardRef((props, ref) => {
+
+  const [isToggled, setIsToggled] = useState(false);
+
+  const toggleOverlayHandler = () => {
+    setIsToggled(!isToggled);
+  };
+
+  // Allows parent component to access functions or methods defined in this component. (expose this function to be used in the parent)
+  useImperativeHandle(ref, () => {
+    return {
+      toggleOverlayHandler: () => {
+        toggleOverlayHandler();
+      },
+    };
+  });
+
+  // Block scroll when overlay is open
+  useEffect(() => {
+    if (isToggled) document.body.style.overflow = "hidden";
+    return () => (document.body.style.overflow = "unset");
+  }, [isToggled]);
+
+  return (
+    <section className={isToggled ? "modal" : "modal modal--hidden"}>
+      <div className="modal__toggle">
+        <button className="modal__toggle-btn" onClick={props.onClick}>
+          <AiOutlineClose />
+        </button>
+      </div>
+      <div className="modal__content">{props.children}</div>
+    </section>
+  );
+});
+
+export default Modal;
