b7591651ec5252a8e5d117e210eae993c6c28dc2
unknown <kinchoily@hotmail.com>
2025-04-09 15:22:27 +0100

    Replace App placeholder with full weather data logic and UI

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
+import { Fragment, useEffect, useRef } from 'react'
+import useWeatherData from './hooks/useWeatherData';
+import Landing from './components/Landing/Landing';
+import Weather from './components/Weather/Weather';
+import Footer from './components/Footer/Footer';
 import './App.scss';
 
 function App() {
+  const {data, error, loading, geoPositionHandler, fetchQueryHandler} = useWeatherData();
+  const isInitialRender = useRef(true);
+ 
+  useEffect(() => {
+    // Stop first useEffect render
+    if (isInitialRender.current) {
+      isInitialRender.current = false;
+      return;
+    }
+    geoPositionHandler();
+  },[]);
+
+  // Data and functions will be passed down via props here
   return (
-    <main className="main">
+    <Fragment>
+      {data && console.log(data)}
+
       {/* Landing */}
+      {!data && <Landing onError={error} handleSubmit={fetchQueryHandler} loading={loading}/>}
 
-      {/* loading screen */}
-
-      {/* Render Weather data */}
-
-      {/* Footer */}
-    </main>
+      {/* Weather data */}
+      {data && <Weather data={data} onError={error} handleSubmit={fetchQueryHandler} loading={loading}/>}
+      <Footer />
+    </Fragment>
   );
 }
 
